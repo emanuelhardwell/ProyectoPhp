a4462cc81cf891a9a86a9566569b1917a8436128
emanuelhardwell <emanuelnoe16@gmail.com>
2020-06-27 20:16:18 -0500

    Se mejoro la clase funciones

--- funciones.php
+++ funciones.php
@@ -20,4 +20,36 @@ function sumaPro($num1, $num2){
 $final= sumaPro(5,11);
 echo 'La suma de 5 + 11 = '.$final;
 
+echo '<br> <br>';
+
+//funciones Pro Pro Pro ...........
+$v1=10;
+$v2=5;
+
+function restar(){
+    global $v1;
+    global $v2;
+    $resultado= $v1 -$v2;
+   
+    echo 'La resta de '. $v1. ' - '. $v2. ' = '.$resultado;
+ }
+
+restar();
+
+echo '<br> <br>';
+
+//funciones Pro Pro Pro ...........
+
+
+function restarPro($v1, $v2){
+    global $v1;
+    global $v2;
+    
+    return $v1 -$v2;
+ }
+
+$resultados= restarPro(18-6);
+echo 'La resta pro de '.$v1. ' - '. $v2. ' = '.$resultados;
+
+
 ?>
